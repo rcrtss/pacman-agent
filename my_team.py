8da9f53ce5d65e2144eaa4fdc8b926b3a9c410f8
rcrtss <rodrigo.cortess@outlook.com>
2026-03-10 19:26:04 +0100

    Offensive and deffensive custom - enhanced

--- my_team.py
+++ my_team.py
@@ -7,6 +7,7 @@
 # For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
 
 import random
+from statistics import median
 import contest.util as util
 
 from contest.capture_agents import CaptureAgent
@@ -21,7 +22,7 @@ import time
 #################
 
 def create_team(first_index, second_index, is_red,
-                first='OffensiveCustomAgent', second='DefensiveReflexAgent', num_training=0):
+                first='OffensiveCustomAgent', second='DefensiveCustomAgent', num_training=0):
     """
     This function should return a list of two agents that will form the
     team, initialized using firstIndex and secondIndex as their agent
@@ -63,9 +64,9 @@ class ReflexCaptureAgent(CaptureAgent):
         actions = game_state.get_legal_actions(self.index)
 
         # You can profile your evaluation time by uncommenting these lines
-        # start = time.time()
+        start = time.time()
         values = [self.evaluate(game_state, a) for a in actions]
-        # print 'eval time for agent %d: %.4f' % (self.index, time.time() - start)
+        print('eval time for agent %d: %.4f' % (self.index, time.time() - start))
 
         max_value = max(values)
         best_actions = [a for a, v in zip(actions, values) if v == max_value]
@@ -193,8 +194,7 @@ class DefensiveReflexAgent(ReflexCaptureAgent):
 class OffensiveCustomAgent(ReflexCaptureAgent):
     """
     A reflex agent focused on offensive strategy that also defends if needed.
-    """
-    
+    """      
     def choose_action(self, game_state):
         """
         Picks best action based on strategy.
@@ -213,22 +213,17 @@ class OffensiveCustomAgent(ReflexCaptureAgent):
         ######################################################################
         
         actions = game_state.get_legal_actions(self.index)
-        agent_info = game_state.get_agent_state(self.index)
         
         # Choose strategy depending on the current game state
         self.aggressive_play_mode = self._should_offense(game_state)
 
         # You can profile your evaluation time by uncommenting these lines
-        start = time.time()
+        # start = time.time()
         best_actions = self.offensive_action(game_state, actions) if self.aggressive_play_mode else self.defensive_action(game_state, actions)            
-        print('eval time for agent %d: %.4f' % (self.index, time.time() - start))
+        # print('eval time for agent %d: %.4f' % (self.index, time.time() - start))
 
-        # Endgame override: choose the action that brings our agent closest to the starting position
-        food_left = len(self.get_food(game_state).as_list())
-        
-        # Win strategy
-        head_back = food_left <= self.FOOD_RETRIEVAL_LIMIT or (agent_info.is_pacman and agent_info.num_carrying > self.MAX_FOOD_CARRY_LIMIT)
-        if head_back:
+        # Head back home if endgame mode, carrying too much food, etc.
+        if self._should_head_back(game_state):
             return self.head_back_strategy(game_state, actions)
 
         return random.choice(best_actions)
@@ -250,6 +245,25 @@ class OffensiveCustomAgent(ReflexCaptureAgent):
         my_state = game_state.get_agent_state(self.index)
         
         if not both_opps_pacman and not opponent_score > self.ENEMY_SCORE_WARNING_LEVEL and my_state.num_carrying <= self.MAX_FOOD_CARRY_LIMIT:
+            return True
+        
+        return False
+    
+    def _should_head_back(self, game_state):
+        """
+        Decide whether or not to head back to the start position to secure points.
+        Returns True if we should head back, False otherwise.
+        
+        TODO: consider a more advanced head back strategy, for example if a ghost is close,
+        we should balance going back with evading the ghost instead of just heading back directly.
+        """
+        agent_info = game_state.get_agent_state(self.index)
+        food_left = len(self.get_food(game_state).as_list())
+        
+        if food_left <= self.FOOD_RETRIEVAL_LIMIT:
+            return True
+        
+        if agent_info.is_pacman and agent_info.num_carrying > self.MAX_FOOD_CARRY_LIMIT:
             return True
         
         return False
@@ -278,7 +292,7 @@ class OffensiveCustomAgent(ReflexCaptureAgent):
                 best_actions.append(action)
         return best_actions
 
-    def _recursive_lookahead(self, game_state, action, depth):
+    def _recursive_lookahead(self, game_state, action, depth=1):
         """
         Recursively evaluates actions up to `depth` levels deep.
         At depth 1, returns the direct evaluation.
@@ -331,19 +345,20 @@ class OffensiveCustomAgent(ReflexCaptureAgent):
     def get_features_off(self, game_state, action):
         features = util.Counter()
         successor = self.get_successor(game_state, action)
+        successor_self_state = successor.get_agent_state(self.index)
         food_list = self.get_food(successor).as_list()
         features['successor_score'] = -len(food_list)  # self.getScore(successor)
 
         # Compute distance to the nearest food
         if len(food_list) > 0:  # This should always be True,  but better safe than sorry
-            my_pos = successor.get_agent_state(self.index).get_position()
+            my_pos = successor_self_state.get_position()
             min_distance = min([self.get_maze_distance(my_pos, food) for food in food_list])
             features['distance_to_food'] = min_distance
         
         # Compute distance to power pill
         power_pills = self.get_capsules(successor)
         if len(power_pills) > 0:
-            my_pos = successor.get_agent_state(self.index).get_position()
+            my_pos = successor_self_state.get_position()
             min_distance = min([self.get_maze_distance(my_pos, power) for power in power_pills])
             features['distance_to_power'] = min_distance
         
@@ -357,7 +372,7 @@ class OffensiveCustomAgent(ReflexCaptureAgent):
         return features
 
     def get_weights_off(self, game_state, action):
-        return {'successor_score': 100, 'distance_to_food': -1, 'distance_to_ghost': 100, 'distance_to_power': -10}
+        return {'successor_score': 100, 'distance_to_food': -5, 'distance_to_ghost': 100, 'distance_to_power': -10}
     
     
     #######################################################################
@@ -373,7 +388,6 @@ class OffensiveCustomAgent(ReflexCaptureAgent):
 
         # Computes whether we're on defense (1) or offense (0)
         features['on_defense'] = 1
-        if self.aggressive_play_mode: features['on_defense'] = 0
 
         # Computes distance to invaders we can see
         enemies = [successor.get_agent_state(i) for i in self.get_opponents(successor)]
@@ -391,3 +405,179 @@ class OffensiveCustomAgent(ReflexCaptureAgent):
 
     def get_weights_def(self, game_state, action):
         return {'num_invaders': -1000, 'on_defense': 100, 'invader_distance': -10, 'stop': -100, 'reverse': -2}
+
+##################################################################################
+#############################  DEFENSIVE AGENT ###################################
+##################################################################################
+class DefensiveCustomAgent(ReflexCaptureAgent):
+    """
+    A reflex agent that keeps its side Pacman-free. Again,
+    this is to give you an idea of what a defensive agent
+    could be like.  It is not the best or only way to make
+    such an agent.
+    """
+    
+    def register_initial_state(self, game_state):
+        super().register_initial_state(game_state)
+        
+        # Compute boundary positions for patrolling when no invaders are present
+        mid_x = game_state.data.layout.width // 2
+        boundary_x = mid_x - 1 if self.red else mid_x
+        walls = game_state.get_walls()
+        height = game_state.data.layout.height
+        self.patrol_points = [(boundary_x, y) for y in range(height) if not walls[boundary_x][y]]
+    
+    def choose_action(self, game_state):
+        """
+        Picks among the actions with the highest Q(s,a).
+        """
+        actions = game_state.get_legal_actions(self.index)
+
+        # You can profile your evaluation time by uncommenting these lines
+        start = time.time()
+        values = [self.evaluate(game_state, a) for a in actions]
+        print('eval time for agent %d: %.4f' % (self.index, time.time() - start))
+
+        max_value = max(values)
+        best_actions = [a for a, v in zip(actions, values) if v == max_value]
+
+        food_left = len(self.get_food(game_state).as_list())
+
+        if food_left <= 2:
+            best_dist = 9999
+            best_action = None
+            for action in actions:
+                successor = self.get_successor(game_state, action)
+                pos2 = successor.get_agent_position(self.index)
+                dist = self.get_maze_distance(self.start, pos2)
+                if dist < best_dist:
+                    best_action = action
+                    best_dist = dist
+            return best_action
+
+        return random.choice(best_actions)
+        
+    def get_features(self, game_state, action):
+        features = util.Counter()
+        successor = self.get_successor(game_state, action)
+
+        my_state = successor.get_agent_state(self.index)
+        my_pos = nearest_point(my_state.get_position())
+
+        # a. Computes whether we're on defense (1) or offense (0). With positive weight, agent is encouraged to defend.
+        features['on_defense'] = 1
+        if my_state.is_pacman: features['on_defense'] = 0
+
+        # Computes distance to invaders we can see
+        enemies = [successor.get_agent_state(i) for i in self.get_opponents(successor)]
+        invaders = [a for a in enemies if a.is_pacman and a.get_position() is not None]
+        
+        # b. num_invaders being penalized means the agent will try to eat invader when possible
+        features['num_invaders'] = len(invaders)
+        if len(invaders) > 0 and my_state.scared_timer == 0:
+            # c. invader_distance neg weight means agent encouraged to get close to invaders.
+            dists = [self.get_maze_distance(my_pos, a.get_position()) for a in invaders]
+            features['invader_distance'] = min(dists)
+
+        if action == Directions.STOP: features['stop'] = 1
+        rev = Directions.REVERSE[game_state.get_agent_state(self.index).configuration.direction]
+        if action == rev: features['reverse'] = 1
+        
+        # d. near_food_cluster to defend vulnerable food clusters
+        food_clusters = self._get_food_clusters(game_state)
+        if food_clusters:
+            dists = [self.get_maze_distance(my_pos, cluster) for cluster in food_clusters]
+            features['distance_to_food_cluster'] = min(dists)
+        
+        # e. distance to recently eaten food to defend against active invaders
+        recently_eaten = self._get_recently_eaten_food(game_state)
+        if recently_eaten:
+            dists = [self.get_maze_distance(my_pos, food) for food in recently_eaten]
+            features['distance_to_eaten_food'] = min(dists)
+            
+        # f. distance to boundary when no invaders to patrol the boundary
+        if not invaders:
+            dists = [self.get_maze_distance(my_pos, pos) for pos in self.patrol_points]
+            features['distance_to_boundary'] = median(dists)
+                
+        # g. run away from pacman if scared
+        if my_state.scared_timer > 0:
+            pacmen = [a for a in enemies if a.is_pacman and a.get_position() is not None]
+            if pacmen:
+                dists = [self.get_maze_distance(my_pos, a.get_position()) for a in pacmen]
+                features['distance_to_pacman'] = min(dists)
+                
+        # h. if opponent is pacman and is against corridor, dont eat them, instead stop there to trap them.
+        
+
+        return features
+
+    def get_weights(self, game_state, action):
+        return {'num_invaders': -2000, 'on_defense': 200, 'invader_distance': -500, 'stop': -200, 'reverse': -40,
+                'distance_to_food_cluster': -4, 'distance_to_eaten_food': -50, 'distance_to_boundary': -3,
+                'distance_to_pacman': 50}
+    
+    def _get_food_clusters(self, game_state):
+        """
+        Returns the positions of food clusters on our side. A food cluster is defined as a group of 4 or more adjacent food pieces.
+        """
+        food = self.get_food_you_are_defending(game_state)
+        food_list = food.as_list()
+        clusters = []
+        visited = set()
+
+        for food_pos in food_list:
+            if food_pos not in visited:
+                cluster = self._bfs_food_cluster(food, food_pos, visited)
+                if len(cluster) > 3:
+                    clusters.append(cluster)
+
+        return [self._get_cluster_center(cluster) for cluster in clusters]
+    
+    def _bfs_food_cluster(self, food, start_pos, visited):
+        """
+        Performs a BFS to find all connected food pieces starting from start_pos.
+        """
+        queue = [start_pos]
+        cluster = []
+        visited.add(start_pos)
+
+        while queue:
+            pos = queue.pop(0)
+            cluster.append(pos)
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                next_pos = (pos[0] + dx, pos[1] + dy)
+                if food[next_pos[0]][next_pos[1]] and next_pos not in visited:
+                    visited.add(next_pos)
+                    queue.append(next_pos)
+
+        return cluster
+    
+    def _get_cluster_center(self, cluster):
+        """
+        Returns the position in the cluster closest to its geometric center.
+        This ensures the returned position is actually on the grid.
+        """
+        x_coords = [pos[0] for pos in cluster]
+        y_coords = [pos[1] for pos in cluster]
+        center_x = sum(x_coords) // len(cluster)
+        center_y = sum(y_coords) // len(cluster)
+        center = (center_x, center_y)
+        
+        # Return the cluster position closest to the calculated center
+        return min(cluster, key=lambda pos: (pos[0] - center[0])**2 + (pos[1] - center[1])**2)
+    
+    def _get_recently_eaten_food(self, game_state):
+        """
+        Returns the position of the most recently eaten food, or None if no food has been eaten.
+        """
+        if self.get_previous_observation() is None:
+            return []
+        
+        current_food = self.get_food_you_are_defending(game_state).as_list()
+        previous_food = self.get_food_you_are_defending(self.get_previous_observation()).as_list()
+        eatenFood = []
+        if len(current_food) < len(previous_food):
+            eatenFood = list(set(previous_food) - set(current_food))
+
+        return eatenFood
